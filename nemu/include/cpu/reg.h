6a3f0067f346570fc79188940c84d4511888dc2f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-05 14:24:05 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-116-generic #117-Ubuntu SMP Fri Aug 28 16:04:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:24:05 up  4:57,  1 user,  load average: 0.87, 0.73, 0.77 dcb9fe7df619bec47188683396f6402ba0260b61

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -21,7 +21,7 @@ typedef struct {
 			uint16_t _16;
 			uint8_t _8[2];
 		} gpr[8];
-		uint32_t eax,ecx,edx,ebx,esp,ebp,esi,edi;
+		struct {uint32_t eax,ecx,edx,ebx,esp,ebp,esi,edi;};
 	};
 	swaddr_t eip;
 
