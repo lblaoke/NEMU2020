91d3106eb18592892e2365354bb9541496f2aea5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-20 08:28:37 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:28:37 up 5 days,  7:53,  1 user,  load average: 1.77, 1.13, 0.86 7449f5e2df4e79e843037481f1b7920398f3d7dd

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -25,3 +25,5 @@
 #include "misc/misc.h"
 
 #include "special/special.h"
+
+#include "con-trans/call.h"
---
+++ nemu/src/cpu/exec/con-trans/call-template.h
@@ -0,0 +1,26 @@
+#include "cpu/exec/template-start.h"
+
+#define instr call
+
+make_helper (concat(call_i_, SUFFIX)) {
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	reg_l(R_ESP)-=DATA_BYTE;
+	swaddr_write(reg_l(R_ESP),4,cpu.eip+len);
+
+	DATA_TYPE_S displacement=op_src->val;
+	cpu.eip+=displacement;
+	print_asm("call %x",cpu.eip+len+1);
+	return len+1;
+}
+make_helper (concat(call_rm_, SUFFIX)) {
+	int len=concat(decode_i_,SUFFIX)(eip+1);
+	reg_l(R_ESP)-=DATA_BYTE;
+	swaddr_write(reg_l(R_ESP),4,cpu.eip+len);
+
+	DATA_TYPE_S displacement=op_src->val;
+	cpu.eip=displacement-len-1;
+	print_asm("call %x",displacement);
+	return len+1;
+}
+
+#include "cpu/exec/template-end.h"
