5a3832108e632b3b9896ab907437abc1a305aaaa
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-20 14:46:30 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:46:30 up  5:45,  1 user,  load average: 0.87, 0.44, 0.46 8c668633fe8217bdc6f76ee1f0fb3830cb615395

--- nemu/src/cpu/exec/con-trans/call-template.h
+++ nemu/src/cpu/exec/con-trans/call-template.h
@@ -12,6 +12,7 @@ make_helper (concat(call_i_, SUFFIX)) {
 	print_asm("call %x",cpu.eip+len+1);
 	return len+1;
 }
+
 make_helper (concat(call_rm_, SUFFIX)) {
 	int len=concat(decode_rm_,SUFFIX)(eip+1);
 	reg_l(R_ESP)-=DATA_BYTE;
