a22281ba7d6931df5b95e4c52f6f24132fad7f4d
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-22 01:59:02 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:59:02 up 11:42,  1 user,  load average: 0.67, 0.72, 0.49 88d576c8bdb9917831a64dc9c4e727301275807c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -13,6 +13,7 @@
 #include "arith/idiv.h"
 #include "arith/div.h"
 #include "arith/cmp.h"
+#include "arith/sub.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,35 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute() {
+	DATA_TYPE result=op_dest->val-op_src->val;
+	int len=(DATA_BYTE<<3)-1;
+
+	cpu.CF=op_dest->val<op_src->val;
+	cpu.SF=result>>len;
+	cpu.ZF=!result;
+
+	int s1=op_dest->val>>len,s2=op_src->val>>len;
+    cpu.OF=(s1!=s2 && s2==cpu.SF) ;
+
+    OPERAND_W(op_dest,result);
+
+	result^=(result>>4);
+	result^=(result>>2);
+	result^=(result>>1);
+	cpu.PF=!(result & 1);
+
+	print_asm_no_template2();
+}
+
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
