e07ba738c8ba21b3b54367a1deb5777d4b715524
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-25 15:17:35 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:17:35 up  1:38,  1 user,  load average: 0.77, 0.53, 0.47 e87859545241e46c9022906382bade5343fd7ee4

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,20 +3,30 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & op_src->val;
-	OPERAND_W(op_dest, result);
+	DATA_TYPE result=op_dest->val & op_src->val;
+	int len=(DATA_BYTE<<3)-1;
 
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
+	cpu.CF=cpu.OF=0;
+	cpu.SF=result>>len;
+    cpu.ZF=!result;
+
+    OPERAND_W(op_dest,result);
+
+	result^=(result>>4);
+	result^=(result>>2);
+	result^=(result>>1);
+	cpu.PF=!(result & 1);
 
 	print_asm_template2();
 }
 
 make_instr_helper(i2a)
 make_instr_helper(i2rm)
-#if DATA_BYTE == 2 || DATA_BYTE == 4
+
+#if DATA_BYTE==2 || DATA_BYTE==4
 make_instr_helper(si2rm)
 #endif
+
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
