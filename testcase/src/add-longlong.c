d401f18ac7c176e158d5b283557e3041036d603f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 08:53:20 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:53:20 up 19:14,  1 user,  load average: 1.02, 0.60, 0.39 ab52c0aaf39253a2dfe9f5ac1d41032ba1815132

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -23,8 +23,7 @@ int main() {
 	}
 */
 	//nemu_assert(loop == NR_DATA * NR_DATA);
-	int i=3,j=3;
-	nemu_assert(add(test_data[i], test_data[j]) == ans[i*NR_DATA+j]);
+	nemu_assert(add(test_data[3], test_data[3]) == ans[27]);
 
 	return 0;
 }
