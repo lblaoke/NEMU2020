3f76030bf29f7a2da1a49cb49d389513229fd820
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 12:06:23 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:06:23 up 22:27,  1 user,  load average: 0.85, 0.72, 0.70 912847a23dd0adc3f6c2740487f4fe64fe3aa6b

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -8,10 +8,10 @@ long long add(long long a, long long b) {
 long long test_data[] = {0, 1, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
 long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0x1LL, 0x2LL, 0x3LL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x2LL, 0x3LL, 0x4LL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0xfffffffffffffffcLL, 0xfffffffffffffffdLL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffdLL, 0xfffffffffffffffeLL};
 
-//#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
+#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))
 
 int main() {
-/*
+
 	int i, j, ans_idx = 0;
 	int loop = 0;
 	for(i = 1; i <NR_DATA; i ++) {
@@ -21,9 +21,8 @@ int main() {
 			loop ++;
 		}
 	}
-*/
-	//nemu_assert(loop == NR_DATA * NR_DATA);
-	nemu_assert(add(test_data[3],test_data[3]) == ans[27]);
+
+	nemu_assert(loop == NR_DATA * NR_DATA);
 
 	return 0;
 }
