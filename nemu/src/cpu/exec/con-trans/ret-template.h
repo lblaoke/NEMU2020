7c72d982d4f43873638cd4b67fb302066a048e7a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 10:11:08 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:11:08 up 20:32,  1 user,  load average: 0.88, 0.63, 0.42 f3939a577b7175ac181645754ec38fd0b3d22805

--- nemu/src/cpu/exec/con-trans/ret-template.h
+++ nemu/src/cpu/exec/con-trans/ret-template.h
@@ -12,12 +12,16 @@ make_helper(concat(ret_n_, SUFFIX)) {
 }
 
 make_helper(concat(ret_i_, SUFFIX)) {
-	int val=instr_fetch(eip+1,2);
+	int i,val=instr_fetch(eip+1,2);
 
 	cpu.eip=MEM_R(REG(R_ESP));
 	if(DATA_BYTE==2) cpu.eip&=0xffff;
 
-	REG(R_ESP)+=DATA_BYTE+val;
+	REG(R_ESP)+=DATA_BYTE;
+
+	for(i=0;i<val;i+=DATA_BYTE) MEM_W(REG(R_ESP)+i,0);
+
+	REG(R_ESP)+=val;
 
 	print_asm("ret $0x%x",val);
 	return 1;
