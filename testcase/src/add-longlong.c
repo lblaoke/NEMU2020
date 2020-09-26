870567bfb247fb7191d89cdd9f36e22d3f68e7ce
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 11:35:17 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:35:17 up 21:56,  1 user,  load average: 0.84, 0.80, 0.69 ca239ededb91d030f00197c85039b4d17f9803fd

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -1,10 +1,10 @@
 #include "trap.h"
-
+/*
 long long add(long long a, long long b) {
 	long long c = a + b;
 	return c;
 }
-
+*/
 //long long test_data[] = {0, 1, 2, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL};
 //long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0x1LL, 0x2LL, 0x3LL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x2LL, 0x3LL, 0x4LL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0x8000000000000001LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x8000000000000000LL, 0x8000000000000001LL, 0x8000000000000002LL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000001LL, 0x8000000000000002LL, 0x8000000000000003LL, 0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffeLL, 0xffffffffffffffffLL, 0LL, 0x7ffffffffffffffdLL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0xfffffffffffffffcLL, 0xfffffffffffffffdLL, 0xffffffffffffffffLL, 0LL, 0x1LL, 0x7ffffffffffffffeLL, 0x7fffffffffffffffLL, 0x8000000000000000LL, 0xfffffffffffffffdLL, 0xfffffffffffffffeLL};
 
@@ -23,7 +23,7 @@ int main() {
 	}
 */
 	//nemu_assert(loop == NR_DATA * NR_DATA);
-	nemu_assert(add(0x7fffffffffffffffLL,0x7fffffffffffffffLL) == 0xfffffffffffffffeLL);
+	nemu_assert(0x7fffffffffffffffLL+0x7fffffffffffffffLL == 0xfffffffffffffffeLL);
 
 	return 0;
 }
