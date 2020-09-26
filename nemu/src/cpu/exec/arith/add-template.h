2bcb7ba7a66f3cfa8c904b052d28d6f59695eecc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 07:35:20 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:35:20 up 17:56,  1 user,  load average: 0.82, 0.72, 0.58 8ce1aca2ef76097e8c4f41a2b0049ef4dd9fb0a2

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
 	DATA_TYPE result=op_dest->val+op_src->val;
 	int len=(DATA_BYTE<<3)-1;
-	int s1=op_dest->val>>len,s2=op_src->val>>len;
+	int s1=(op_dest->val)>>len,s2=(op_src->val)>>len;
 
 	cpu.CF=(result < op_dest->val);
 	cpu.SF=result>>len;
