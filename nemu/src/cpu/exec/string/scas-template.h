2f5500855c0c67505ea28404997f35f766f2f465
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 11:00:36 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:00:36 up 6 days, 21:21,  1 user,  load average: 0.91, 0.62, 0.35 6b5f7b62d619b9abe29062c9f845d1be8d6b253

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -8,15 +8,15 @@ static void do_execute() {
 	switch(DATA_BYTE) {
 		case 1:
 			a=reg_w(R_AL);
-			dest=swaddr_read(reg_w(R_EDI),DATA_BYTE);
+			dest=swaddr_read(reg_l(R_EDI),DATA_BYTE);
 			break;
 		case 2:
 			a=reg_w(R_AX);
 			dest=swaddr_read(reg_w(R_DI),DATA_BYTE);
 			break;
 		default:
-			a=reg_w(R_EAX);
-			dest=swaddr_read(reg_w(R_EDI),DATA_BYTE);
+			a=reg_l(R_EAX);
+			dest=swaddr_read(reg_l(R_EDI),DATA_BYTE);
 	}
 	DATA_TYPE result=a-dest;
 
