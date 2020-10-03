2f85febe364619fd0a3a031fb5ae77243b4cc00c
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 14:17:15 +0000

    >  run 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:17:15 up 1 day,  2:19,  1 user,  load average: 0.63, 0.69, 0.64 26de05b6e6c935a72b3cce50118b36d3ec23ebb8

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -9,6 +9,11 @@ int a[8]={0,1,0,1,0,1,0,1};
 int b[8]={0,0,1,1,0,0,1,1};
 int c[8]={0,0,0,0,1,1,1,1};
 
+int f(int x) {
+	if(x<=0) return x;
+	return 1+f(x-1);
+}
+
 int main() {
 	int i;
 
@@ -26,6 +31,8 @@ int main() {
 		nemu_assert(a[i]/2==(a[i]>>1));
 		nemu_assert(a[i]-a[i]==0);
 	}
+
+	for(i=0;i<8;i++) nemu_assert(i==f(i));
 
 	memset(buf,0xf,sizeof(buf));
 	buf[127]=0;
