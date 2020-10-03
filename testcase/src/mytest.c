38eb5c7205309fabae9ad6e52ae75b1eadba1ef7
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-03 14:02:21 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:02:21 up 1 day,  2:04,  1 user,  load average: 0.98, 0.65, 0.51 db58ba9a6b5cdc495bf7c9ea3ec449692d1d4b7

---
+++ testcase/src/mytest.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "trap.h"
+
+char buf[128];
+char str[128]="mytest";
+
+bool a[]={0,1,0,1,0,1,0,1};
+bool b[]={0,0,1,1,0,0,1,1};
+bool c[]={0,0,0,0,1,1,1,1};
+
+int main() {
+	int i;
+
+	for(i=0;i<8;i++) {
+		nemu_assert(!(a[i] && b[i])==(!a[i] || !b[i]));
+		nemu_assert(!(a[i] || b[i])==(!a[i] && !b[i]));
+		nemu_assert(!!a[i]==a[i])
+		nemu_assert(a[i] && (b[i] || c[i])==(a[i] && b[i]) || (a[i] && c[i]));
+	}
+
+	memset(buf,0,sizeof(buf));
+	nemu_assert(strlen(buf)==128)
+	strcpy(buf,str);
+	nemu_assert(!strcmp(str,"mytest"));
+
+	return 0;
+}
