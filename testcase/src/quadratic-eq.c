8879887ea676306b54c3103fa3eb9f7365ec3303
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 08:06:17 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:06:17 up  1:13,  1 user,  load average: 1.05, 0.86, 0.73 c044dc3bc103cfd988202601da52a0e39c0d1ffd

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -11,14 +11,14 @@ int main() {
 	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
 	FLOAT sqrt_dt = sqrt(dt);
 
-	//FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
-	//FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
+	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
+	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
 
-	//FLOAT x1_ans = f2F(0.618);
-	//FLOAT x2_ans = f2F(-1.618);
+	FLOAT x1_ans = f2F(0.618);
+	FLOAT x2_ans = f2F(-1.618);
 
-	//nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
-	//nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
+	nemu_assert(Fabs(x1_ans - x1) < f2F(1e-4));
+	nemu_assert(Fabs(x2_ans - x2) < f2F(1e-4));
 
 	return 0*(sqrt_dt);
 }
