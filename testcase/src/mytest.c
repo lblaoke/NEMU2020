e4bdf9b7c7ba708db08b9eadf077884902636ea6
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 02:51:50 +0000

    >  run 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:51:50 up  7:54,  1 user,  load average: 0.66, 0.18, 0.06 66f6fedf50026337c88a6ea8c9a187baa85a03

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -52,9 +52,9 @@ int main() {
 	strcpy(buf,str);
 	nemu_assert(!strcmp(str,"mytest"));
 */
-	sprintf(buf, "%x",F_div_F(int2F(4),int2F(2)));
+	//sprintf(buf, "%x",F_div_F(int2F(4),int2F(2)));
 	//sprintf(buf, "%x",f2F(2.0));
-	nemu_assert(!strcmp(buf, "10000"));
+	nemu_assert(int2F(2)==0x20000);
 
 	//FLOAT f1=int2F(4);
 	//nemu_assert(F_div_F(f1,int2F(2))==int2F(2));
