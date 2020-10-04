521538bc4a972c5c9405f56fc4c9b31e02671464
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 15:53:43 +0000

    >  compile kernel 3018205377 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:53:43 up  9:01,  1 user,  load average: 0.58, 0.81, 0.74 d55d171d6c7bab8a94932b61d9865cb13900fe3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -31,25 +31,26 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0xBadC0de;
+	const uint32_t elf_magic=0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	panic("please implement me");
-	for(; true; ) {
+	ph=(void *)(buf+elf->e_phoff);
+	int i;
+	for(i=0;i<elf->e_phnum;i++,ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			 ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
+			 memset((void*)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
