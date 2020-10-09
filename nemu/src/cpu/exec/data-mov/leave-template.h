600b000773da5454f6684b9b3091dfd36e1e71ea
3018205377-Li Bolian <lblaoke@foxmail.com>
2020-10-09 16:08:55 +0000

    PA3 start!

---
+++ nemu/src/cpu/exec/data-mov/leave-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+
+#define instr leave
+
+static void do_execute () {
+	swaddr_t i;
+	for(i=REG(R_ESP);i<REG(R_EBP);i+=DATA_BYTE) MEM_W(i,0);
+
+	REG(R_ESP)=REG(R_EBP);
+	REG(R_EBP)=MEM_R(REG(R_ESP));
+	REG(R_ESP)+=DATA_BYTE;
+	
+	print_asm("leave");
+}
+
+make_instr_helper(n)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -0,0 +1,25 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movsx
+
+static void do_movsx_b() {
+	DATA_TYPE_S result=(int8_t)(op_src->val & 0xff);
+	OPERAND_W(op_dest,result);
+
+	print_asm_template2();
+}
+static void do_movsx_w() {
+	DATA_TYPE_S result=(int16_t)(op_src->val & 0xffff);
+	OPERAND_W(op_dest,result);
+	
+	print_asm_template2();
+}
+
+make_helper(movsx_rm2r_b) {
+	return idex(eip,decode_rm2r_b, do_movsx_b);
+}
+make_helper(movsx_rm2r_w) {
+	return idex(eip,decode_rm2r_w, do_movsx_w);
+}
+
+#include "cpu/exec/template-end.h"
