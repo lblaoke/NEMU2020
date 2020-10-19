e8779d08606a9aa4da91887b68170ade05f7b2e6
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-19 09:35:36 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:35:36 up 1 min,  1 user,  load average: 1.16, 0.37, 0.13 23caf10b1a43b4f36a06a33b914d8fcfdda5e1f8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -6,9 +6,9 @@
 #define GROUP_WIDTH1 7
 #define IN_WIDTH1 3
 
-#define NR_DATA 64
-#define NR_GROUP1 128
-#define NR_IN1 8
+#define NR_DATA (1 << DATA_WIDTH)
+#define NR_GROUP1 (1 << GROUP_WIDTH1)
+#define NR_IN1 (1 << IN_WIDTH1)
 
 void ddr3_read(hwaddr_t, void *);
 uint32_t dram_read(hwaddr_t, size_t);
@@ -17,57 +17,51 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
 struct Cache1 {
-	uint8_t data[NR_DATA];
-	uint32_t tag;
 	bool valid;
-} cache1[NR_GROUP1][NR_IN1];
+	uint32_t tag;
+	uint8_t data[NR_DATA];
+} cache1[NR_GROUP1*NR_IN1];
 
 uint32_t TAG,GROUP,OFFSET;
 
 void init_cache() {
-	uint32_t i, j;
-	for(i = 0; i < NR_GROUP1; i ++) {
-		for(j = 0; j < NR_IN1; j ++) {
-			memset(cache1[i][j].data,0,NR_DATA);
-			cache1[i][j].tag=0;
-			cache1[i][j].valid=false;
-		}
-	}
+	uint32_t block;
+	for(block=0;block<NR_GROUP1*NR_IN1;block++) cache1[block].valid=false;
 }
 
 uint32_t cache1_read(hwaddr_t addr) {
 	srand(0);
 	bool success=false;
-	uint32_t in,i;
-	for(in=0;in<NR_IN1;in++) if(cache1[GROUP][in].valid && cache1[GROUP][in].tag==TAG) {
+	uint32_t block,i;
+	for(block=GROUP*NR_IN1;block<(GROUP+1)*NR_IN1;block++) if(cache1[block].valid && cache1[block].tag==TAG) {
 		success=true;
 		break;
 	}
 
 	if(!success) {
-		for(in=0;in<NR_IN1 && cache1[GROUP][in].valid;in++);
+		for(block=GROUP*NR_IN1;block<(GROUP+1)*NR_IN1 && cache1[block].valid;block++);
 		
-		if(in>=NR_IN1) in=rand()%NR_IN1;
+		if(block>=(GROUP+1)*NR_IN1) block=GROUP*NR_IN1+rand()%NR_IN1;
 
 		uint32_t start=((addr>>DATA_WIDTH)<<DATA_WIDTH);
-		for(i=0;i<NR_DATA;i+=BURST_LEN) ddr3_read(start+i,cache1[GROUP][in].data+i);
-		cache1[GROUP][in].tag=TAG;
-		cache1[GROUP][in].valid=true;
+		for(i=0;i<NR_DATA;i+=BURST_LEN) ddr3_read(start+i,cache1[block].data+i);
+		cache1[block].tag=TAG;
+		cache1[block].valid=true;
 	}
 
-	return in;
+	return block;
 }
 
 void cache1_write(hwaddr_t addr,size_t len,uint32_t buf) {
 	bool success=false;
-	uint32_t in;
-	for(in=0;in<NR_IN1;in++) if(cache1[GROUP][in].valid && cache1[GROUP][in].tag==TAG) {
+	uint32_t block;
+	for(block=GROUP*NR_IN1;block<(GROUP+1)*NR_IN1;block++) if(cache1[block].valid && cache1[block].tag==TAG) {
 		success=true;
 		break;
 	}
 
 	dram_write(addr,len,buf);
-	if(success) memcpy(cache1[GROUP][in].data+OFFSET,&buf,len);
+	if(success) memcpy(cache1[block].data+OFFSET,&buf,len);
 }
 
 uint32_t hwaddr_read(hwaddr_t addr,size_t len) {
@@ -81,13 +75,13 @@ uint32_t hwaddr_read(hwaddr_t addr,size_t len) {
 	memset(temp,0,sizeof(temp));
 
 	if(OFFSET + len >= NR_DATA) {
-		memcpy(temp,cache1[GROUP][block1].data + OFFSET, NR_DATA - OFFSET);
+		memcpy(temp,cache1[block1].data + OFFSET, NR_DATA - OFFSET);
 
 		GROUP=((addr+len)>>DATA_WIDTH) & (NR_GROUP1-1);
 		uint32_t block2=cache1_read(addr+len);
 
-		memcpy(temp + NR_DATA - OFFSET,cache1[GROUP][block2].data, len - (NR_DATA - OFFSET));
-	} else memcpy(temp,cache1[GROUP][block1].data + OFFSET,len);
+		memcpy(temp + NR_DATA - OFFSET,cache1[block2].data, len - (NR_DATA - OFFSET));
+	} else memcpy(temp,cache1[block1].data + OFFSET,len);
 
 	uint32_t zero=0;
 	return unalign_rw(temp+zero, 4) & (~0u >> ((4 - len) << 3));
