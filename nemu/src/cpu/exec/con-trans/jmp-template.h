1735cd054d1e45b93a05d7b09e3774ac2b521b27
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 17:24:54 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:54 up 1 day,  7:50,  1 user,  load average: 2.65, 1.80, 1.22 1abd65897299f81b931fba13be1932841bd550d9

--- nemu/src/cpu/exec/con-trans/jmp-template.h
+++ nemu/src/cpu/exec/con-trans/jmp-template.h
@@ -1,7 +1,7 @@
 #include "cpu/exec/template-start.h"
+#include "../../../../../lib-common/x86-inc/mmu.h"
 
 #define instr jmp
-
 
 static void do_execute() {
 	DATA_TYPE_S displacement=op_src->val;
@@ -16,6 +16,33 @@ static void do_execute() {
 		print_asm("jmp %x",cpu.eip+1+DATA_BYTE);
 	}
 }
+
+#if DATA_BYTE == 4
+make_helper(ljmp) {
+	SegDesc seg;
+	SegDesc *seg_des=&seg;
+
+	uint32_t op1 = instr_fetch(eip+1, 4)-7;
+	uint16_t op2 = instr_fetch(eip +5, 2);
+	cpu.eip = op1;
+	cpu.cs.selector = op2;
+
+	Assert(((cpu.cs.selector>>3)<<3) <= cpu.gdtr.seg_limit, "segment out of limit %d, %d", ((cpu.cs.selector>>3)<<3), cpu.gdtr.seg_limit);
+	seg_des->_0 = instr_fetch(cpu.gdtr.base_addr + ((cpu.cs.selector>>3)<<3), 4);
+	seg_des->_4 = instr_fetch(cpu.gdtr.base_addr + ((cpu.cs.selector>>3)<<3)+4, 4);
+
+	Assert(seg_des->present == 1, "segment error!");
+	cpu.cs.seg_base1 = seg_des->base_15_0;
+	cpu.cs.seg_base2 = seg_des->base_23_16;
+	cpu.cs.seg_base3 = seg_des->base_31_24;
+	cpu.cs.seg_limit1 = seg_des->limit_15_0;
+	cpu.cs.seg_limit2 = seg_des->limit_19_16;
+	cpu.cs.seg_limit3 = 0xfff;
+
+	print_asm("ljmp %x,%x", op2, op1+7);
+	return 7;
+}
+#endif
 
 make_instr_helper(i)
 make_instr_helper(rm)
