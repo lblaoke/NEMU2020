e9f678198234c291bb480524ca87a424f47fbf3b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 05:04:44 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:04:44 up 19:30,  1 user,  load average: 0.69, 0.81, 0.70 8556f0e8bc6dfdc5884b746946dee047cc06ae8a

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -40,7 +40,7 @@ uint32_t cache2_read(Address addr) {
 		srand(block);
 		block=start+rand()%NR_IN2;
 		if(cache2[block].dirty) {
-			printf("write back%d\n",block);
+			printf("write back: %d\n",block);
 			uint8_t mask[BURST_LEN<<1];
 			memset(mask,1,BURST_LEN<<1);
 
@@ -85,6 +85,7 @@ void cache2_write(Address addr,size_t len,uint32_t buf) {
 	}
 
 	if(block>=end) block=cache2_read(addr);
+	printf("write: %d\n",block);
 
 	cache2[block].dirty=true;
 
