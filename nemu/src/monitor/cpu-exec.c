053962812d1169f7e505310300cfd9ab023af1ca
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 07:47:53 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:47:53 up 1 day, 22:13,  1 user,  load average: 0.69, 0.47, 0.31 3d1f73a69bfd4a4c9895e7539429045e572f3df7

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -63,7 +63,7 @@ void cpu_exec(volatile uint32_t n) {
 		//printf("%ld\n",(long int)&cpu.eip);
 		//printf("1\n");
 		int instr_len = exec(cpu.eip);
-		printf("%d\n",cpu.eip);
+		//printf("%d\n",cpu.eip);
 		//printf("%d\n",instr_len);
 		//printf("2\n");
 		//printf("%ld\n",(long int)&cpu.eip);
@@ -74,7 +74,7 @@ void cpu_exec(volatile uint32_t n) {
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
-		//Log_write("%s\n", asm_buf);
+		Log_write("%s\n", asm_buf);
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
