9a745b26fdf7d67060106cb386acdb4b4557bdc6
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-17 08:13:04 +0000

    >  compile kernel 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:13:04 up 1 day, 22:15,  1 user,  load average: 0.73, 0.60, 0.42 b739d78736c1492d82682a34d2e8ed3e965b654

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -14,6 +14,10 @@ static void sys_ioctl(TrapFrame *tf) {
 	tf->eax = fs_ioctl(tf->ebx, tf->ecx, (void *)tf->edx);
 }
 
+static void sys_write(TrapFrame *tf) {
+	
+}
+
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
 		/* The `add_irq_handle' system call is artificial. We use it to
@@ -29,8 +33,7 @@ void do_syscall(TrapFrame *tf) {
 
 		case SYS_brk: sys_brk(tf); break;
 		case SYS_ioctl: sys_ioctl(tf); break;
-
-		/* TODO: Add more system calls. */
+		case SYS_write: sys_write(tf); break;
 
 		default: panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
 	}
