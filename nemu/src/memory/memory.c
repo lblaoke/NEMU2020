1829e7e7312ef8f37547dd39ea8b12a9134c6a93
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-23 02:54:09 +0000

    >  compile NEMU 3018205377 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:54:09 up  1:30,  1 user,  load average: 0.94, 0.73, 0.67 920048bc7af4ed89ae04dcf4e8ec88859a862330

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -39,7 +39,7 @@ hwaddr_t page_translate(lnaddr_t addr) {
 		B.OFFSET=(A.PAGE<<2);
 		page_1.val = hwaddr_read(B.address,4);
 
-		Assert(page_1.present, "page do not exist at %x", cpu.eip);
+		//Assert(page_1.present, "page do not exist at %x", cpu.eip);
 		A.tag = page_1.page_frame;
 		B.address = addr;
 		B.offset = 0;
